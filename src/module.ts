c7412ba54bff0b7eef076526ccd595e45cd4557b
2552895864 <2552895864@qq.com>
2020-02-05 20:59:45 +0800

    change to PanelCtrl

--- src/module.ts
+++ src/module.ts
@@ -1,67 +1,27 @@
-import { MetricsPanelCtrl } from 'grafana/app/plugins/sdk';
-import defaultsDeep from 'lodash/defaultsDeep';
+import { PanelCtrl } from 'grafana/app/plugins/sdk';
+import _ from 'lodash';
 
-import { DataFrame } from '@grafana/data';
 
-interface KeyValue {
-  key: string;
-  value: any;
-}
-
-export default class SimpleCtrl extends MetricsPanelCtrl {
+export default class SimpleCtrl extends PanelCtrl {
   static templateUrl = 'partials/module.html';
 
   panelDefaults = {
     text: 'Hello World',
   };
 
-  // Simple example showing the last value of all data
-  firstValues: KeyValue[] = [];
 
   /** @ngInject */
   constructor($scope, $injector) {
     super($scope, $injector);
-    defaultsDeep(this.panel, this.panelDefaults);
-
-    // Get results directly as DataFrames
-    (this as any).dataFormat = 'series';
+    _.defaultsDeep(this.panel, this.panelDefaults);
 
     this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
-    this.events.on('render', this.onRender.bind(this));
-    this.events.on('data-error', this.onDataError.bind(this));
+    this.events.on('panel-initialized', this.render.bind(this));
+    this.events.on('component-did-mount', this.render.bind(this));
   }
 
   onInitEditMode() {
     this.addEditorTab('Options', `public/plugins/${this.pluginId}/partials/options.html`, 2);
-  }
-
-  onRender() {
-    if (!this.firstValues || !this.firstValues.length) {
-      return;
-    }
-
-    // Tells the screen capture system that you finished
-    this.renderingCompleted();
-  }
-
-  onDataError(err: any) {
-    console.log('onDataError', err);
-  }
-
-  // 6.3+ get typed DataFrame directly
-  handleDataFrame(data: DataFrame[]) {
-    const values: KeyValue[] = [];
-
-    for (const frame of data) {
-      for (let i = 0; i < frame.fields.length; i++) {
-        values.push({
-          key: frame.fields[i].name,
-          value: frame.fields[i].values,
-        });
-      }
-    }
-
-    this.firstValues = values;
   }
 }
 

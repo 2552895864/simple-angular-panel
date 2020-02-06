6cff3412798215db8482dc7bf4fa39ca2a6cb406
2552895864 <2552895864@qq.com>
2020-02-06 10:37:24 +0800

    add data handle

--- src/module.ts
+++ src/module.ts
@@ -1,13 +1,19 @@
-import { PanelCtrl } from 'grafana/app/plugins/sdk';
+import { MetricsPanelCtrl } from 'grafana/app/plugins/sdk';
 import _ from 'lodash';
+import { DataFrame } from '@grafana/data';
 
+interface KeyValue {
+  key: string;
+  value: any;
+}
 
-export default class SimpleCtrl extends PanelCtrl {
+export default class SimpleCtrl extends MetricsPanelCtrl {
   static templateUrl = 'partials/module.html';
 
   panelDefaults = {
     text: 'Hello World',
   };
+  firstValues: KeyValue[] = [];
 
 
   /** @ngInject */
@@ -15,13 +21,45 @@ export default class SimpleCtrl extends PanelCtrl {
     super($scope, $injector);
     _.defaultsDeep(this.panel, this.panelDefaults);
 
+    (this as any).dataFormat = 'series';
+
     this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
     this.events.on('panel-initialized', this.render.bind(this));
     this.events.on('component-did-mount', this.render.bind(this));
+    this.events.on('render', this.onRender.bind(this));
+    this.events.on('data-error', this.onDataError.bind(this));
   }
 
   onInitEditMode() {
     this.addEditorTab('Options', `public/plugins/${this.pluginId}/partials/options.html`, 2);
+  }
+
+  onRender() {
+    if (!this.firstValues || !this.firstValues.length) {
+      return;
+    }
+    // Tells the screen capture system that you finished
+    this.renderingCompleted();
+  }
+
+  onDataError(err: any) {
+    console.log('onDataError', err);
+  }
+
+  // 6.3+ get typed DataFrame directly
+  handleDataFrame(data: DataFrame[]) {
+    const values: KeyValue[] = [];
+    console.log(data);
+    for (const frame of data) {
+      for (let i = 0; i < frame.fields.length; i++) {
+        values.push({
+          key: frame.fields[i].name,
+          value: frame.fields[i].values,
+        });
+      }
+    }
+
+    this.firstValues = values;
   }
 }
 

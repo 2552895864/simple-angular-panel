779198643e3d00922aa5c65a5d93810e923b7a9a
2552895864 <2552895864@qq.com>
2020-02-10 17:34:27 +0800

    debug angular dom

--- src/module.ts
+++ src/module.ts
@@ -77,7 +77,7 @@ export default class SimpleCtrl extends MetricsPanelCtrl {
       this.mapHeight = this.height;
       this.mapWidth = WIDTH_HEIGHT_RATE * this.height;
     }
-    const circles = window.angular.element('.circle');
+    const circles = (window as any).angular.element('.circle');
     console.log(circles);
     this.render();
   }

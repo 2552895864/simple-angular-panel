c2d09401d02ac5dc0e75bfb31211183d974bb804
2552895864 <2552895864@qq.com>
2020-02-11 15:56:00 +0800

    debug text box style

--- src/utils/makePrompt.js
+++ src/utils/makePrompt.js
@@ -2,12 +2,14 @@ export default function makePrompt(x, y, name, live, host, container, isLeft){
     var textBox = document.createElement('div');
     var nameBox = document.createElement('div');
     var countBox = document.createElement('div');
+    var length = String(name + live + host).length;
     textBox.classList.add('text-box');
     nameBox.classList.add('name-box');
     countBox.classList.add('count-box');
+    textBox.style.width = `${length}rem`;
     textBox.style.left = `${x}px`;
     textBox.style.top = `${y}px`;
-    textBox.style.transform = `translate3d(calc(${isLeft? -100 : 0}% + ${isLeft ? -2 : 2}rem), -50%, 0)`;
+    textBox.style.transform = `translate3d(${isLeft? -100 : 0}%), -50%, 0)`;
     nameBox.innerText = name;
     countBox.innerText = `${live} / ${host}`;
     textBox.appendChild(nameBox);
